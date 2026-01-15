0fd6e603f63ebff17a5403c4d523099ad7d510c5
Saifobai <saifobaidi.s22@gmail.com>
2026-01-15 02:55:59 +0100

    updated_deploy_01

--- backend/index.js
+++ backend/index.js
@@ -128,11 +128,12 @@ app.use(
     express.static(path.join(__dirname, "../client/dist"))
 );
 
-app.get("*", (req, res) => {
-    res.sendFile(
-        path.join(__dirname, "../client/dist/index.html")
-    );
-});
+// app.get("/*", (req, res) => {
+//     res.sendFile(
+//         path.join(__dirname, "../client/dist/index.html")
+//     );
+// });
+
 
 // ======================
 // START SERVER (HOSTINGER SAFE)
